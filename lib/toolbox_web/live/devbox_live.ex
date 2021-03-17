2bc701f0b34a245b3c2a483852941175d4612a1d
Matthew Thompson <matthew.thompson.a@gmail.com>
2021-03-17 02:38:35 +0000

    status interval now that csrf token working

--- lib/toolbox_web/live/devbox_live.ex
+++ lib/toolbox_web/live/devbox_live.ex
@@ -3,46 +3,11 @@ defmodule ToolboxWeb.DevBoxLive do
   alias Toolbox.AWS.EC2
 
   @devbox_id Application.get_env(:toolbox, :debox_id, nil)
-
-  # @impl true
-  # def mount(_params, _session, socket) do
-  #   {:ok, assign(socket, query: "", results: %{})}
-  # end
-
-  # @impl true
-  # def handle_event("suggest", %{"q" => query}, socket) do
-  #   {:noreply, assign(socket, results: search(query), query: query)}
-  # end
-
-  # @impl true
-  # def handle_event("search", %{"q" => query}, socket) do
-  #   case search(query) do
-  #     %{^query => vsn} ->
-  #       {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
-
-  #     _ ->
-  #       {:noreply,
-  #        socket
-  #        |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-  #        |> assign(results: %{}, query: query)}
-  #   end
-  # end
-
-  # defp search(query) do
-  #   if not ToolboxWeb.Endpoint.config(:code_reloader) do
-  #     raise "action disabled when not in development"
-  #   end
-
-  #   for {app, desc, vsn} <- Application.started_applications(),
-  #       app = to_string(app),
-  #       String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-  #       into: %{},
-  #       do: {app, vsn}
-  # end
+  @status_interval 1000
 
   def mount(_params, _session, socket) do
     if connected?(socket) do
-      :timer.send_interval(10_000, self(), :status)
+      :timer.send_interval(@status_interval, self(), :status)
     end
 
     {:ok, assign(socket, devbox: EC2.instance_by_id(@devbox_id))}
